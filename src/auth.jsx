38519f68ba6bc591b47857a834cd3713f9acb59a
Mikel Losada <mikel.losada@gmail.com>
2020-06-24 21:44:40 +0200

    Prevent refresh token from being destroyed after it's used

--- src/auth.jsx
+++ src/auth.jsx
@@ -3,12 +3,29 @@ import {createAuthProvider} from 'react-token-auth';
 export const API_URL = 'http://127.0.0.1:8000';
 const REFRESH_TOKEN_URL = `${API_URL}/api/token/refresh/`;
 
+
+const jsonHeaders = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json'
+};
+
+
 export const [useAuth, authFetch, login, logout] =
   createAuthProvider({
     accessTokenKey: 'access',
-    onUpdateToken: (token) => fetch(REFRESH_TOKEN_URL, {
+    onUpdateToken: (token) => fetch(REFRESH_TOKEN_URL, {      
       method: 'POST',
-      body: token.refreshToken
+      body: JSON.stringify({refresh: token.refresh}),
+      headers: jsonHeaders,
     })
       .then(r => r.json())
+      .then((r) => {
+	return {
+	  "refresh": token.refresh,
+	  "access": r.access
+	};
+      }
+           )
   });
+
+
